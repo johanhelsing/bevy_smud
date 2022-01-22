879501ce5eb48f2a9e0a91c55ef4bd978609b0c2
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-01-22 15:00:40 +0100

    add mesh manual example from bevy

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,6 +1,7 @@
 use bevy::{
     asset::AssetServerSettings,
     prelude::*,
+    render::{mesh::Indices, render_resource::PrimitiveTopology},
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
 use bevy_so_smooth::*;
@@ -14,7 +15,8 @@ fn main() {
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_plugin(SoSmoothPlugin)
-        .add_startup_system(setup)
+        // .add_startup_system(setup)
+        .add_startup_system(star)
         .run();
 }
 
@@ -36,4 +38,76 @@ fn setup(
     });
 
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn star(
+    mut commands: Commands,
+    // We will add a new Mesh for the star being created
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    // Let's define the mesh for the object we want to draw: a nice star.
+    // We will specify here what kind of topology is used to define the mesh,
+    // that is, how triangles are built from the vertices. We will use a
+    // triangle list, meaning that each vertex of the triangle has to be
+    // specified.
+    let mut star = Mesh::new(PrimitiveTopology::TriangleList);
+
+    // Vertices need to have a position attribute. We will use the following
+    // vertices (I hope you can spot the star in the schema).
+    //
+    //        1
+    //
+    //     10   2
+    // 9      0      3
+    //     8     4
+    //        6
+    //   7        5
+    //
+    // These vertices are specificed in 3D space.
+    let mut v_pos = vec![[0.0, 0.0, 0.0]];
+    for i in 0..10 {
+        // Angle of each vertex is 1/10 of TAU, plus PI/2 for positioning vertex 0
+        let a = std::f32::consts::FRAC_PI_2 - i as f32 * std::f32::consts::TAU / 10.0;
+        // Radius of internal vertices (2, 4, 6, 8, 10) is 100, it's 200 for external
+        let r = (1 - i % 2) as f32 * 100.0 + 100.0;
+        // Add the vertex coordinates
+        v_pos.push([r * a.cos(), r * a.sin(), 0.0]);
+    }
+    // Set the position attribute
+    star.set_attribute(Mesh::ATTRIBUTE_POSITION, v_pos);
+    // And a RGB color attribute as well
+    let mut v_color = vec![[0.0, 0.0, 0.0, 1.0]];
+    v_color.extend_from_slice(&[[1.0, 1.0, 0.0, 1.0]; 10]);
+    star.set_attribute(Mesh::ATTRIBUTE_COLOR, v_color);
+
+    // Now, we specify the indices of the vertex that are going to compose the
+    // triangles in our star. Vertices in triangles have to be specified in CCW
+    // winding (that will be the front face, colored). Since we are using
+    // triangle list, we will specify each triangle as 3 vertices
+    //   First triangle: 0, 2, 1
+    //   Second triangle: 0, 3, 2
+    //   Third triangle: 0, 4, 3
+    //   etc
+    //   Last triangle: 0, 1, 10
+    let mut indices = vec![0, 1, 10];
+    for i in 2..=10 {
+        indices.extend_from_slice(&[0, i, i - 1]);
+    }
+    star.set_indices(Some(Indices::U32(indices)));
+
+    // We can now spawn the entities for the star and the camera
+    commands.spawn_bundle((
+        // We use a marker component to identify the custom colored meshes
+        SmudShape::default(),
+        // The `Handle<Mesh>` needs to be wrapped in a `Mesh2dHandle` to use 2d rendering instead of 3d
+        Mesh2dHandle(meshes.add(star)),
+        // These other components are needed for 2d meshes to be rendered
+        Transform::default(),
+        GlobalTransform::default(),
+        Visibility::default(),
+        ComputedVisibility::default(),
+    ));
+    commands
+        // And use an orthographic projection
+        .spawn_bundle(OrthographicCameraBundle::new_2d());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,92 @@
 use bevy::{
+    core::FloatOrd,
+    core_pipeline::Transparent2d,
     ecs::system::{lifetimeless::SRes, SystemParamItem},
     prelude::*,
     reflect::TypeUuid,
     render::{
-        render_asset::{PrepareAssetError, RenderAsset},
+        render_asset::{PrepareAssetError, RenderAsset, RenderAssets},
+        render_phase::{AddRenderCommand, DrawFunctions, RenderPhase, SetItemPipeline},
         render_resource::{
             std140::{AsStd140, Std140},
             BindGroup, BindGroupDescriptor, BindGroupEntry, BindGroupLayout,
-            BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingType, Buffer,
-            BufferBindingType, BufferInitDescriptor, BufferSize, BufferUsages, ShaderStages,
+            BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingType, BlendState, Buffer,
+            BufferBindingType, BufferInitDescriptor, BufferSize, BufferUsages, ColorTargetState,
+            ColorWrites, Face, FragmentState, FrontFace, MultisampleState, PolygonMode,
+            PrimitiveState, RenderPipelineCache, RenderPipelineDescriptor, ShaderStages,
+            SpecializedPipeline, SpecializedPipelines, TextureFormat, VertexAttribute,
+            VertexBufferLayout, VertexFormat, VertexState, VertexStepMode,
         },
         renderer::RenderDevice,
+        texture::BevyDefault,
+        view::VisibleEntities,
+        RenderApp, RenderStage,
     },
-    sprite::{Material2d, Material2dPipeline, Material2dPlugin},
+    sprite::{
+        DrawMesh2d, Material2d, Material2dPipeline, Material2dPlugin, Mesh2dHandle, Mesh2dPipeline,
+        Mesh2dPipelineKey, Mesh2dUniform, SetMesh2dBindGroup, SetMesh2dViewBindGroup,
+    },
 };
 
 pub struct SoSmoothPlugin;
 
+const SMUD_MESH2D_SHADER_HANDLE: HandleUntyped =
+    HandleUntyped::weak_from_u64(Shader::TYPE_UUID, 5645555317811706725);
+
+// Needed to keep the shaders alive
+struct ShaderHandles {
+    #[allow(dead_code)]
+    smud_shader: Handle<Shader>,
+}
+
 impl Plugin for SoSmoothPlugin {
     fn build(&self, app: &mut App) {
+        // TODO: remove
         app.add_plugin(Material2dPlugin::<CustomMaterial>::default());
+
+        let shader: Handle<Shader> = app
+            .world
+            .get_resource::<AssetServer>()
+            .unwrap()
+            .load("smud.wgsl");
+
+        app.world
+            .get_resource_mut::<Assets<Shader>>()
+            .unwrap()
+            .add_alias(&shader, SMUD_MESH2D_SHADER_HANDLE);
+
+        app.insert_resource(ShaderHandles {
+            smud_shader: shader,
+        });
+
+        // non-hot-reload path
+        // app.world
+        //     .get_resource_mut::<Assets<Shader>>()
+        //     .unwrap()
+        //     .set_untracked(
+        //         SMUD_MESH2D_SHADER_HANDLE,
+        //         Shader::from_wgsl(include_str!("../assets/smud.wgsl")),
+        //     );
+
+        let render_app = app.get_sub_app_mut(RenderApp).unwrap();
+        render_app
+            // TODO: what does it do internally?
+            .add_render_command::<Transparent2d, DrawSmudShape>()
+            .init_resource::<SmudPipeline>()
+            .init_resource::<SpecializedPipelines<SmudPipeline>>()
+            .add_system_to_stage(RenderStage::Extract, extract_shapes)
+            .add_system_to_stage(RenderStage::Queue, queue_shapes);
     }
 }
+
+type DrawSmudShape = (
+    SetItemPipeline,
+    // Set the view uniform as bind group 0
+    SetMesh2dViewBindGroup<0>,
+    // Set the mesh uniform as bind group 1
+    SetMesh2dBindGroup<1>,
+    DrawMesh2d,
+);
 
 #[derive(Debug, Clone, TypeUuid, AsStd140)]
 #[uuid = "200dfa11-9b87-40e1-8774-ffcd3fcb17df"]
@@ -96,5 +161,158 @@ impl RenderAsset for CustomMaterial {
             _buffer: buffer,
             bind_group,
         })
+    }
+}
+
+#[derive(Component, Default)]
+pub struct SmudShape;
+
+struct SmudPipeline {
+    mesh2d_pipeline: Mesh2dPipeline,
+}
+
+impl FromWorld for SmudPipeline {
+    fn from_world(world: &mut World) -> Self {
+        Self {
+            mesh2d_pipeline: FromWorld::from_world(world),
+        }
+    }
+}
+
+impl SpecializedPipeline for SmudPipeline {
+    type Key = Mesh2dPipelineKey;
+
+    fn specialize(&self, key: Self::Key) -> RenderPipelineDescriptor {
+        // Customize how to store the meshes' vertex attributes in the vertex buffer
+        // Our meshes only have position and color
+        let vertex_attributes = vec![
+            // Position (GOTCHA! Vertex_Position isn't first in the buffer due to how Mesh sorts attributes(alphabetically))
+            VertexAttribute {
+                format: VertexFormat::Float32x3,
+                // this offset is the size of the color attribute, which is stored first
+                offset: 16,
+                // position is available at location 0 in the shader
+                shader_location: 0,
+            },
+            //Color
+            VertexAttribute {
+                format: VertexFormat::Float32x4,
+                offset: 0,
+                shader_location: 1,
+            },
+        ];
+        // This is the sum of the size of position and color attributes (12 + 16 = 28)
+        let vertex_array_stride = 28;
+
+        RenderPipelineDescriptor {
+            vertex: VertexState {
+                shader: SMUD_MESH2D_SHADER_HANDLE.typed::<Shader>(),
+                entry_point: "vertex".into(),
+                shader_defs: Vec::new(),
+                buffers: vec![VertexBufferLayout {
+                    array_stride: vertex_array_stride,
+                    step_mode: VertexStepMode::Vertex,
+                    attributes: vertex_attributes,
+                }],
+            },
+            fragment: Some(FragmentState {
+                shader: SMUD_MESH2D_SHADER_HANDLE.typed::<Shader>(),
+                entry_point: "fragment".into(),
+                shader_defs: Vec::new(),
+                targets: vec![ColorTargetState {
+                    format: TextureFormat::bevy_default(),
+                    blend: Some(BlendState::ALPHA_BLENDING),
+                    write_mask: ColorWrites::ALL,
+                }],
+            }),
+            // Use the two standard uniforms for 2d meshes
+            layout: Some(vec![
+                // Bind group 0 is the view uniform
+                self.mesh2d_pipeline.view_layout.clone(),
+                // Bind group 1 is the mesh uniform
+                self.mesh2d_pipeline.mesh_layout.clone(),
+            ]),
+            primitive: PrimitiveState {
+                front_face: FrontFace::Ccw,
+                cull_mode: Some(Face::Back),
+                unclipped_depth: false, // What is this?
+                polygon_mode: PolygonMode::Fill,
+                conservative: false, // What is this?
+                topology: key.primitive_topology(),
+                strip_index_format: None, // TODO: what does this do?
+            },
+            depth_stencil: None,
+            multisample: MultisampleState {
+                count: key.msaa_samples(),
+                mask: !0,                         // what does the mask do?
+                alpha_to_coverage_enabled: false, // what is this?
+            },
+            label: Some("smud_pipeline".into()),
+        }
+    }
+}
+
+fn extract_shapes(
+    mut commands: Commands,
+    mut previous_len: Local<usize>,
+    query: Query<(Entity, &ComputedVisibility), With<SmudShape>>,
+) {
+    let mut values = Vec::with_capacity(*previous_len);
+    for (entity, computed_visibility) in query.iter() {
+        if !computed_visibility.is_visible {
+            continue;
+        }
+        // TODO: copy over other data as well?
+        values.push((entity, (SmudShape,)));
+    }
+    *previous_len = values.len();
+    commands.insert_or_spawn_batch(values);
+}
+
+fn queue_shapes(
+    mut views: Query<(&mut RenderPhase<Transparent2d>, &VisibleEntities)>,
+    mut pipelines: ResMut<SpecializedPipelines<SmudPipeline>>,
+    mut pipeline_cache: ResMut<RenderPipelineCache>,
+    transparent_draw_functions: Res<DrawFunctions<Transparent2d>>,
+    smud_pipeline: Res<SmudPipeline>,
+    shape_query: Query<(&Mesh2dHandle, &Mesh2dUniform), With<SmudShape>>,
+    msaa: Res<Msaa>,
+    render_meshes: Res<RenderAssets<Mesh>>,
+) {
+    if shape_query.is_empty() {
+        return;
+    }
+    // Iterate over each view (a camera is a view)
+    for (mut transparent_phase, visible_entities) in views.iter_mut() {
+        let draw_smud_shape = transparent_draw_functions
+            .read()
+            .get_id::<DrawSmudShape>()
+            .unwrap();
+
+        let mesh_key = Mesh2dPipelineKey::from_msaa_samples(msaa.samples);
+
+        for visible_entity in &visible_entities.entities {
+            if let Ok((mesh2d_handle, mesh2d_uniform)) = shape_query.get(*visible_entity) {
+                // Get our specialized pipeline
+                let mesh = render_meshes
+                    .get(&mesh2d_handle.0)
+                    .expect("Shape without mesh");
+                let mesh_key =
+                    mesh_key | Mesh2dPipelineKey::from_primitive_topology(mesh.primitive_topology);
+
+                let pipeline_id =
+                    pipelines.specialize(&mut pipeline_cache, &smud_pipeline, mesh_key);
+
+                let mesh_z = mesh2d_uniform.transform.w_axis.z;
+
+                transparent_phase.add(Transparent2d {
+                    entity: *visible_entity,
+                    draw_function: draw_smud_shape,
+                    pipeline: pipeline_id,
+                    sort_key: FloatOrd(mesh_z),
+                    batch_range: None, // TODO: use this?
+                });
+            }
+        }
     }
 }
