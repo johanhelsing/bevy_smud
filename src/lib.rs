360541e4def635405fecd94756291f5f1c3044e3
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-05-20 19:44:59 +0200

    style: Remove commented out code

--- src/lib.rs
+++ src/lib.rs
@@ -475,10 +475,7 @@ fn extract_shapes(
             transform: *transform,
             sdf_shader: shape.sdf.clone_weak(),
             fill_shader: shape.fill.clone_weak(),
-            frame
-            // rect: None,
-            // // Pass the custom size
-            // custom_size: shape.custom_size,
+            frame,
         });
     }
 }
@@ -709,37 +706,3 @@ struct TimeMeta {
 struct TimeUniform {
     seconds_since_startup: f32,
 }
-
-// TODO: is RenderAsset asking too much?
-// pub trait SdfShapeShader: 'static + Send + Sync {
-//     /// Shader must include a handle to a shader with a wgsl function with signature fn distance(pos: vec2<f32>) -> f32
-//     fn shader(asset_server: &AssetServer) -> Handle<Shader>;
-// }
-
-// /// Adds the necessary ECS resources and render logic to enable rendering entities using the given [`SdfShapeShader`]
-// /// asset type
-// pub struct SdfShapePlugin<S: SdfShapeShader>(PhantomData<S>);
-
-// impl<S: SdfShapeShader> Default for SdfShapePlugin<S> {
-//     fn default() -> Self {
-//         Self(default())
-//     }
-// }
-
-// impl<S: SdfShapeShader> Plugin for SdfShapePlugin<S> {
-//     fn build(&self, app: &mut App) {
-//         // TODO:
-//         // app.add_asset::<S>()
-//         //     .add_plugin(ExtractComponentPlugin::<Handle<S>>::default())
-//         //     .add_plugin(RenderAssetPlugin::<S>::default());
-//         // if let Ok(render_app) = app.get_sub_app_mut(RenderApp) {
-//         //     render_app
-//         //         .add_render_command::<Transparent3d, DrawMaterial<S>>()
-//         //         .add_render_command::<Opaque3d, DrawMaterial<S>>()
-//         //         .add_render_command::<AlphaMask3d, DrawMaterial<S>>()
-//         //         .init_resource::<MaterialPipeline<S>>()
-//         //         .init_resource::<SpecializedPipelines<MaterialPipeline<S>>>()
-//         //         .add_system_to_stage(RenderStage::Queue, queue_material_meshes::<S>);
-//         // }
-//     }
-// }
