b25782809d93b5da755d549607cbf32b161755df
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-11-13 12:37:22 +0100

    docs: Update hot reloading example for Bevy 0.9

--- examples/hot_reload.rs
+++ examples/hot_reload.rs
@@ -4,20 +4,21 @@ use bevy_smud::prelude::*;
 fn main() {
     App::new()
         .insert_resource(Msaa { samples: 4 })
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            watch_for_changes: true,
+            ..default()
+        }))
         .add_plugin(SmudPlugin)
         .add_startup_system(setup)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    asset_server.watch_for_changes().unwrap();
-
     // When sdfs are loaded from files, hot reloading works as normal
     // Open up assets/bevy.wgsl and make some changes and see them reflected when you save
     let bevy = asset_server.load("bevy.wgsl");
 
-    commands.spawn_bundle(ShapeBundle {
+    commands.spawn(ShapeBundle {
         transform: Transform {
             scale: Vec3::splat(0.4),
             ..default()
@@ -33,5 +34,5 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         ..default()
     });
 
-    commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle::default());
 }
