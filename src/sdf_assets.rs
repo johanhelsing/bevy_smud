2eb252e78f7f84816aaecdc2558e942a84528a23
Dima Rets <Dmytro.Rets@Gamesys.co.uk>
2023-07-07 00:39:36 +0100

    dbg

--- src/sdf_assets.rs
+++ src/sdf_assets.rs
@@ -18,7 +18,7 @@ impl SdfAssets for Assets<Shader> {
         let shader = Shader::from_wgsl(
             format!(
                 r#"
-#import bevy_smud::shapes Shapes
+#import "shapes.wgsl"
 fn sdf(p: vec2<f32>) -> f32 {{
     {body}
 }}
