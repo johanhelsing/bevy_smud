a9892101b69a4b97cba55507fdb458e69b551d03
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2023-08-11 15:21:08 +0200

    Port params example to bevy 0.11

--- examples/params.rs
+++ examples/params.rs
@@ -11,27 +11,25 @@ use rand::{prelude::IteratorRandom, random};
 
 fn main() {
     App::new()
+        .add_state::<GameState>()
         .add_loading_state(
-            LoadingState::new(GameState::Loading)
-                .continue_to_state(GameState::Running)
-                .with_collection::<AssetHandles>(),
+            LoadingState::new(GameState::Loading).continue_to_state(GameState::Running),
         )
-        .add_state(GameState::Loading)
-        .insert_resource(Msaa { samples: 4 })
-        .add_plugins(DefaultPlugins)
-        .add_plugin(SmudPlugin)
-        .add_plugin(bevy_lospec::PalettePlugin)
-        .add_system_set(SystemSet::on_enter(GameState::Running).with_system(setup))
+        .add_collection_to_loading_state::<_, AssetHandles>(GameState::Loading)
+        .insert_resource(Msaa::Off)
+        .add_plugins((DefaultPlugins, SmudPlugin, bevy_lospec::PalettePlugin))
+        .add_systems(OnEnter(GameState::Running), setup)
         .run();
 }
 
-#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+#[derive(Clone, Eq, PartialEq, Debug, Hash, States, Default)]
 enum GameState {
+    #[default]
     Loading,
     Running,
 }
 
-#[derive(AssetCollection)]
+#[derive(Resource, AssetCollection)]
 struct AssetHandles {
     #[asset(path = "vinik24.json")]
     palette: Handle<bevy_lospec::Palette>,
@@ -43,7 +41,7 @@ fn setup(
     assets: Res<AssetHandles>,
     palettes: Res<Assets<bevy_lospec::Palette>>,
 ) {
-    let box_sdf = shaders.add_sdf_expr("sd_box(p, params.xy)");
+    let box_sdf = shaders.add_sdf_expr("smud::sd_box(p, params.xy)");
     let padding = 5.; // need some padding for the outline/falloff
     let spacing = 70.;
     let palette = palettes.get(&assets.palette).unwrap();
@@ -70,7 +68,7 @@ fn setup(
             .copied()
             .unwrap_or(Color::PINK);
 
-        commands.spawn_bundle(ShapeBundle {
+        commands.spawn(ShapeBundle {
             transform,
             shape: SmudShape {
                 color,
@@ -83,5 +81,5 @@ fn setup(
         });
     }
 
-    commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle::default());
 }
