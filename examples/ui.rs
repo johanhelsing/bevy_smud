c7c6bc89b511499ad12bddba195d442c0ba4f89f
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2025-10-16 00:47:54 +0200

    docs: cleanup ui example

--- examples/ui.rs
+++ examples/ui.rs
@@ -10,23 +10,22 @@ fn main() {
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    info!("Setup: spawning UI test nodes");
+    commands.spawn(Camera2d);
 
     let circle_sdf = asset_server.load("shapes/circle.wgsl");
     let ellipse_sdf = asset_server.load("shapes/ellipse.wgsl");
 
     // Container for all elements
-    commands
-        .spawn(Node {
+    commands.spawn((
+        Node {
             width: Val::Percent(100.0),
             height: Val::Percent(100.0),
             justify_content: JustifyContent::Center,
             align_items: AlignItems::Center,
             ..default()
-        })
-        .with_children(|stack| {
-            // Mix circles and ellipses - this should reproduce the artifacts!
-            stack.spawn((
+        },
+        children![
+            (
                 Node {
                     position_type: PositionType::Absolute,
                     left: Val::Px(50.0),
@@ -41,9 +40,8 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     params: Vec4::new(100.0, 0.0, 0.0, 0.0),
                     ..default()
                 },
-            ));
-
-            stack.spawn((
+            ),
+            (
                 Node {
                     position_type: PositionType::Absolute,
                     left: Val::Px(100.0),
@@ -58,9 +56,8 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     params: Vec4::new(100.0, 50.0, 0.0, 0.0),
                     ..default()
                 },
-            ));
-
-            stack.spawn((
+            ),
+            (
                 Node {
                     position_type: PositionType::Absolute,
                     left: Val::Px(150.0),
@@ -75,8 +72,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     params: Vec4::new(100.0, 0.0, 0.0, 0.0),
                     ..default()
                 },
-            ));
-        });
-
-    commands.spawn(Camera2d);
+            )
+        ],
+    ));
 }
